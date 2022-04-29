101128efe8068341bc6bae3605486543227c9b7a
yonataba <yonataba@post.bgu.ac.il>
2022-04-29 16:44:52 +0300

    warmup

--- Warmup.java
+++ Warmup.java
@@ -2,17 +2,47 @@
 
 public class Warmup {
     public static int backtrackingSearch(int[] arr, int x, int forward, int back, Stack myStack) {
-        // TODO: implement your code here
-    	return -987; // temporal return command to prevent compilation error 
+        int index =0;
+        while (index < arr.length){
+            for (int j=0; j < forward; j++, index ++){
+                if (index >= arr.length)
+                    return -1;
+                if (arr[index] == x)
+                    return index;
+                else
+                    myStack.push(arr[index]);
+            }
+            for (int j =index ;index > j-back; index = index-1){
+                myStack.pop();
+            }
+        }
+
+    	return -1;
     }
 
     public static int consistentBinSearch(int[] arr, int x, Stack myStack) {
-        // TODO: implement your code here
+        int left =0, right = arr.length-1;
+        myStack.push(left);
+        myStack.push(right);
+        while (left < right ){
+            int middle = (left + right) /2;
+            if (arr[middle] == x)
+                return middle;
+            if (arr[middle] < x)
+                left = middle +1;
+            else
+                right = middle -1;
+            int inconsistencies = Consistency.isConsistent(arr);
+            for (int j=0; j< inconsistencies; j++){
+                right = (int) myStack.pop();
+                left = (int) myStack.pop();
+            }
+        }
     	
-    	// Your implementation should contain a this line:
-    	int inconsistencies = Consistency.isConsistent(arr);
+    	// Your implementation should contain this line:
+    	//int inconsistencies = Consistency.isConsistent(arr);
     	
-    	return -987; // temporal return command to prevent compilation error
+    	return -1; // temporal return command to prevent compilation error
     }
     
 }
---
+++ main.java
@@ -0,0 +1,10 @@
+
+
+public class main {
+    public static void main(String[] args) {
+        int[] a ={1, 1, 2, 14, 15, 16, 23, 99, 100, 100, 100, 132, 193, 196, 197};
+        Stack stack = new Stack();
+        System.out.println(a[11]);
+      // System.out.println(Warmup.consistentBinSearch(a,132, stack));
+    }
+}
